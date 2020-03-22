def1c88087634b6dca224b3ed87f80e38e2842a5
imtiaz <programmer.imtiaz@gmail.com>
2020-03-22 12:58:27 +0600

    ADD- retrofit and okhttp client instance creation added into Appmodule

--- app/src/main/java/com/imtiaz/androidboilerplates/di/AppComponent.kt
+++ app/src/main/java/com/imtiaz/androidboilerplates/di/AppComponent.kt
@@ -2,13 +2,20 @@ package com.imtiaz.androidboilerplates.di
 
 import android.app.Application
 import com.imtiaz.androidboilerplates.di.module.ActivityBuilderModule
+import com.imtiaz.androidboilerplates.di.module.AppModule
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AndroidSupportInjectionModule::class, ActivityBuilderModule::class])
+@Component(
+    modules = [
+    AndroidSupportInjectionModule::class,
+    ActivityBuilderModule::class,
+    AppModule::class
+    ]
+)
 interface AppComponent {
 
     @Component.Builder
--- app/src/main/java/com/imtiaz/androidboilerplates/di/module/AppModule.kt
+++ app/src/main/java/com/imtiaz/androidboilerplates/di/module/AppModule.kt
@@ -1,9 +1,42 @@
 package com.imtiaz.androidboilerplates.di.module
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.imtiaz.androidboilerplates.utils.BASE_URL
+import com.imtiaz.androidboilerplates.utils.TIME_OUT
 import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 abstract class AppModule {
-    
+
+    //Room, SharedPref will be here..
+
+    @Singleton
+    @Provides
+    fun provideGson() : Gson = GsonBuilder().setLenient().create()
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient() : OkHttpClient{
+        return OkHttpClient().newBuilder()
+            .writeTimeout(TIME_OUT, TimeUnit.SECONDS)
+            .readTimeout(TIME_OUT, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(client: OkHttpClient, gson: Gson) : Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
 }
