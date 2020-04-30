84d834f9721cad4cf490ac0e0bf1cbc8b70b0ebe
imtiaz <programmer.imtiaz@gmail.com>
2020-04-30 18:10:51 +0600

    Update- MainFragment support inject added

--- app/src/main/java/com/imtiaz/androidboilerplates/ui/MainFragment.kt
+++ app/src/main/java/com/imtiaz/androidboilerplates/ui/MainFragment.kt
@@ -1,22 +1,38 @@
 package com.imtiaz.androidboilerplates.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.imtiaz.androidboilerplates.R
+import dagger.android.support.AndroidSupportInjection
+import retrofit2.Retrofit
+import javax.inject.Inject
 
 class MainFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    @Inject lateinit var retrofit: Retrofit
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidSupportInjection.inject(this)
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if(retrofit != null){
+            Log.e("retrofitNull", "No")
+        }
+        else{
+            Log.e("retrofitNull", "Yes")
+        }
     }
 
 }
